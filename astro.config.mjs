bc9e84aa2fb4f34e0bf785f4a4743c6bfc95b4e9
TofikJS <tofikbedru2009@gmail.com>
2025-02-01 20:09:38 +0300

    tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  })],
+  site: 'https://astro-tofik.netlify.app/'
+});
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <Button onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </Button>
+    </div>
+  );
+}
